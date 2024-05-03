fc9a7ff2c32721208a15d4b8b392907c624624a2
PhucLe03 <hailua112003@gmail.com>
2024-05-03 21:07:16 +0700

    Minor update

--- Backend/app.py
+++ Backend/app.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 # from typing import Annotated
+import mysql.connector as sqlcon
+import json
 
 from SmartCom.face_validator import Face_Validator
 
 be_app  = FastAPI()
+
+database = sqlcon.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="",
+    database="test"
+)
+
+db_cursor = database.cursor()
 
 origins = [
     "http://localhost",
@@ -41,6 +53,10 @@ class UserLogin(BaseModel):
     email: str
     password: str = Form()
     
+class User(BaseModel):
+    name: str
+    login_info: UserLogin
+    
 # class UserLoginForm(BaseModel):
 #     message: str
 #     loginUser: UserLogin
@@ -52,6 +68,19 @@ class UserLogin(BaseModel):
 
 @be_app.post("/api/auth/signin", response_model=UserLogin)
 async def signin(email: str = Form(), password: str = Form()):
-    item = UserLogin(email=email, password=password)
-    return item
-    return JSONResponse(jsonable_encoder(item))
+    # item = UserLogin(email=email, password=password)
+    try:
+        sql = "SELECT * FROM user WHERE email = %s AND password= %s"
+        db_cursor.execute(sql, (str(email), str(password)))
+        db_res = db_cursor.fetchone()
+        final_res = {}
+        for i, col in enumerate(db_cursor.description):
+            # print('col', col[0])
+            final_res[col[0]] = db_res[i]
+        # print('res', final_res)
+        final_res['accessToken'] = True
+        # NOTE: return json object of the user
+        return JSONResponse(jsonable_encoder(final_res))
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Email or password is incorrect")
+
